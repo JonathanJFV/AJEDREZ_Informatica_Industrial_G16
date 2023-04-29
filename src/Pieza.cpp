60552617da81e6aa6e9b12ecbce3a02b83f1789e
Alvarorico <61272141+Alvarorico@users.noreply.github.com>
2023-04-29 11:53:23 +0200

    Update Pieza.cpp

--- src/Pieza.cpp
+++ src/Pieza.cpp
@@ -1,3 +1,14 @@
 #include "Pieza.h"
 #include "freeglut.h"
 
+void Pieza::setPos(int x, int y)
+{
+  x = posx;
+  y = posy;
+}
+
+float Pieza::getPos(float pos)
+{
+  if (pos == 1) return x;
+	if (pos == 2) return y;
+}
