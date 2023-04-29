20a4229a4673502b9bf55d386437c7c77fff2054
Alvarorico <61272141+Alvarorico@users.noreply.github.com>
2023-04-29 10:42:37 +0200

    Update Pieza.h

--- src/Pieza.h
+++ src/Pieza.h
@@ -2,8 +2,15 @@
 
 class Pieza
 {
-
-public:
-
+  int posx;
+  int posy;
+  char colorp;
+ public:
+  Pieza();
+  Pieza(int x, int y, char c);
+  virtual ~Pieza();
+  void setPos();
+  void getPos();
+  void getColor();
 };
 
