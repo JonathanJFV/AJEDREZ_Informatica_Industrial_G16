37eadb501cbd1c175b7828b0f039ff7946765ac2
ningxinye <61113927+ningxinye@users.noreply.github.com>
2023-05-02 02:00:19 +0200

    Update Pieza.h

--- src/Pieza.h
+++ src/Pieza.h
@@ -1,16 +1,39 @@
-#pragma once
+//es la clase base de todas las piezas 
+
+#ifndef PIEZA_H
+#define PIEZA_H
+
+#include "ETSIDI.h"
+#include "freeglut.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+using namespace ETSIDI;
 
 class Pieza
 {
-private:
+protected:
+  bool seleccionado;
   int posx;
   int posy;
-  char colorp;
+  TIPO tipo;
+	COLOR Color;
+  
  public:
-  Pieza();
-  virtual ~Pieza();
-  void setPos(int x, int y);
-  float getPos(float pos);
-  char getColor();
+//color de la pieza
+  enum COLOR { NS = -1, BLANCO, NEGRO };
+  //tipo de la pieza
+	enum TIPO{NF = -1, PEON};
+  
+    Pieza();
+    Pieza(TIPO t, COLOR c, int x, int y);
+   
+  	virtual void print();
+	  virtual void dibujar();
+	  virtual bool mover(int x, int y);//new
+	  virtual bool seleccionar(int x,int y);
+  
+
 };
 
