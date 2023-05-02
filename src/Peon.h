db0f2b91575c6ce28f0bebc1b7028cb0f5777d1f
ningxinye <61113927+ningxinye@users.noreply.github.com>
2023-05-02 02:07:08 +0200

    Update Peon.h

--- src/Peon.h
+++ src/Peon.h
@@ -2,6 +2,25 @@
 #define PEON_H
 #include "Pieza.h"
 
+class Peon:public Pieza  
+{
+private:
+//con la libreria de etsidi añade el dibujo de PEON
+	SpriteSequence sprite_peon_blanco{ "imagenes/peon_blanco.png", 1 };
+	SpriteSequence sprite_peon_negro{ "imagenes/peon_negro.png", 1 };
+  
+public:
+	Peon();
+	Peon(Figura::COLOR c, int x = -1, int y = -1);
+	virtual ~Peon();
+	void print();
+	void dibujar();
+	bool mover(int x, int y);//new
+	bool seleccionar(int x, int y);
+};
+
+
+
 
 
 #endif
