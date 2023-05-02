ca3b72112ae39acd5ca2dd0109179f1c29093d01
ningxinye <61113927+ningxinye@users.noreply.github.com>
2023-05-02 02:17:40 +0200

    Create casilla.h

---
+++ src/casilla.h
@@ -0,0 +1,26 @@
+//lista para gestionar las piezas
+#ifndef CASILLA_H
+#define CASILLA_H
+
+class Casilla
+{
+private:
+	int tam;
+	Pieza* piez[MAX];
+public:
+	Casilla() :tam(0)
+	{
+		for (int i = 0;i < MAX;i++)
+			piez[i] = NULL;
+	}
+	virtual ~Casilla();
+	bool agregar(Pieza* p);
+	void dibujar();
+	void destruirContenido();
+	Pieza* seleccionar(int x, int y);//nuevo
+	bool mover(int x,int y);//nuevo
+};
+
+
+
+#endif
