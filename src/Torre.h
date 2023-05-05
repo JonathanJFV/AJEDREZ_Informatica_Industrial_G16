fca8bb97daf28669177eaa5ad98f63655e7cbf2b
PabloSanchz <80348065+PabloSanchz@users.noreply.github.com>
2023-05-05 12:48:40 +0200

    Update Torre.h

--- src/Torre.h
+++ src/Torre.h
@@ -6,8 +6,8 @@ class Torre : public Pieza
 {
 private:
     //con la libreria de etsidi añade el dibujo de TORRE
-    SpriteSequence sprite_caballo_blanco{ "bin/imagenes/torre_blanco.png", 1 };
-  	SpriteSequence sprite_caballo_negro{ "bin/imagenes/torre_negro.png", 1 };
+    SpriteSequence sprite_torre_blanco{ "bin/imagenes/torre_blanco.png", 1 };
+  	SpriteSequence sprite_torre_negro{ "bin/imagenes/torre_negro.png", 1 };
 public:
   Torre();
 	Torre(Pieza::COLOR c, int x = -1, int y = -1);
