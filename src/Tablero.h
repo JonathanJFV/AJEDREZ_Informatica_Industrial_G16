d678ef174f31b5088edd71c4c94ca1cc9f8e98e1
Alvarorico <61272141+Alvarorico@users.noreply.github.com>
2023-05-16 15:31:01 +0200

    Update Tablero.h

--- src/Tablero.h
+++ src/Tablero.h
@@ -1,27 +1,33 @@
 #pragma once
-#include "Mundo.h"
+#include "Rey.h"
+#include "Torre.h"
+#include "Alfil.h"
+#include "Reina.h"
+#include "Caballo.h"
+#include "Peon.h"
+#include "ETSIDI.h"
 
-#define FILAS 8
-#define COLUMNAS 8
+
+#define N_FILAS 8
+#define N_COLUMNAS 8
 
 class Tablero
 {
-private:
-	Pieza* t[FILAS][COLUMNAS];
+protected:
+	int tam;
+	Pieza* tab[N_FILAS][N_COLUMNAS];
 	unsigned char rojo, _rojo;
 	unsigned char verde, _verde;
 	unsigned char azul, _azul;
 
 public:
-	Pieza* Comidas[30];
-	int PComidas = 0;
-	Pieza* operator[](Casilla pos) const { return t[pos.fila][pos.columna]; }
-	void cambioEstado(Casilla inicio, Casilla final);
-	void movimientoEstado(Casilla inicio, Casilla final);
+	Tablero();
+	Pieza* operator[](Pieza pos) const { return tab[pos.fila][pos.columna]; }
 	// funcion principal que dibuja el Tablero
 	void dibuja();
 	void setColor(unsigned char r, unsigned char v, unsigned char a,
 		unsigned char _r, unsigned char _v, unsigned char _a);
 	
 };
+
 
