26def6b215c3a109c2769f15862485e2328cbcd4
Alvarorico <61272141+Alvarorico@users.noreply.github.com>
2023-05-16 14:53:03 +0200

    Update Pieza.h

--- src/Pieza.h
+++ src/Pieza.h
@@ -1,11 +1,8 @@
-//es la clase base de todas las piezas 
-
 #ifndef PIEZA_H
 #define PIEZA_H
 
 #include "ETSIDI.h"
 #include "freeglut.h"
-#include "Casilla.h"
 #include <string>
 #include <iostream>
 
@@ -13,33 +10,30 @@ using namespace std;
 using namespace ETSIDI;
 
 class Tablero;
-
 class Pieza
 {
-
-  
  public:
 //color de la pieza
-  enum COLOR { NS = -1, BLANCO, NEGRO };
+  enum COLOR { NS = -1, BLANCA, NEGRA };
   //tipo de la pieza
 	enum TIPO{NF = -1, PEON, ALFIL, CABALLO, REINA, REY, TORRE};
-  
+	int fila = 0, columna = 0;
+
     Pieza();
     Pieza(TIPO t, COLOR c, int x, int y);
-    TIPO getTipo() { return tipo; }
-    COLOR getColor() { return Color; }
-   
-    virtual void print();
-    virtual void dibujar();
-    virtual bool mover(Casilla incio, Casilla final, Tablero& t);//new
-    virtual bool seleccionar(int x,int y);
+	Pieza(int fila, int columna) :fila(fila), columna(columna) {}
+  	  virtual void print();
+	  virtual void dibujar();
+	  //virtual bool seleccionar(int x,int y);
+	  static bool mLineal(Pieza inicio, Pieza fin);
+	  static bool mDiagonal(Pieza inicio, Pieza fin);
   
 protected:
   bool seleccionado;
   int posx;
   int posy;
-  TIPO tipo = NF;
-  COLOR Color = NS;
+  TIPO tipo;
+  COLOR Color;
 
 };
 
