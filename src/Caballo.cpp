62435a599f52935c87241f913745c2355c3bb519
Alvarorico <61272141+Alvarorico@users.noreply.github.com>
2023-05-17 16:24:21 +0200

    Update Caballo.cpp

--- src/Caballo.cpp
+++ src/Caballo.cpp
@@ -8,11 +8,11 @@ Caballo::Caballo()
 	posy = -1;
 	seleccionado = false;
 
-	//un gráfico de caballo blanco
+	//un grÃ¡fico de caballo blanco
 	sprite_caballo_blanco.setCenter(1, 0);
 	sprite_caballo_blanco.setSize(0.9, 0.9);
 
-	//un gráfico de caballo negro
+	//un grÃ¡fico de caballo negro
 	sprite_caballo_negro.setCenter(1, 0);
 	sprite_caballo_negro.setSize(0.9, 0.9);
 }
@@ -23,11 +23,11 @@ Caballo::Caballo(Pieza::COLOR c, int x, int y)
 	posx = x;
 	posy = y;
 	seleccionado = false;
-	//un gráfico de caballo blanco
+	//un grÃ¡fico de caballo blanco
 	sprite_caballo_blanco.setCenter(1, 0);
 	//como z de la pieza es 1, se establece una relacion entre casilla y pieza de 1:0.9
 	sprite_caballo_blanco.setSize(0.9, 0.9);
-	//un gráfico de caballo negro
+	//un grÃ¡fico de caballo negro
 	sprite_caballo_negro.setCenter(1, 0);
 	sprite_caballo_negro.setSize(0.9, 0.9);
 }
@@ -43,10 +43,10 @@ void Caballo::print()
 	string color;
 	switch (Color)
 	{
-	case Pieza::BLANCO:
+	case Pieza::BLANCA:
 		color = "BLANCO";
 		break;
-	case Pieza::NEGRO:
+	case Pieza::NEGRA:
 		color = "NEGRO";
 		break;
 	default:
@@ -57,21 +57,21 @@ void Caballo::print()
 
 void Caballo::dibujar()
 {
-	//compara todos los piezas, solo hay que cambiar el gráfico
-	if (Color == Pieza::BLANCO)
+	//compara todos los piezas, solo hay que cambiar el grÃ¡fico
+	if (Color == Pieza::BLANCA)
 	{
 		//dibujar un caballo blanco
 		glPushMatrix();
 		glTranslatef(1.325 + posx * 0.925, 0.325 + posy * 0.925, 1);
 		/*z = 1 para que la figura se ponga por encima del tablero,
-					(1.325, 0.325, 1) es la posición de la primera casilla para el sprite que estamos usado ,
+					(1.325, 0.325, 1) es la posiciÃ³n de la primera casilla para el sprite que estamos usado ,
 					+-0.925 es la el valor hay que sumar en x e/u y para cambiar de casilla */
 		glColor3f(1.0f, 0.0f, 0.0f);
 		sprite_caballo_blanco.draw();
 		glPopMatrix();
 	}
 
-	if (Color == Pieza::NEGRO)
+	if (Color == Pieza::NEGRA)
 	{
 		//dibujar un caballo negro
 		glPushMatrix();
